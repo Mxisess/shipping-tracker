e2b2d1e58c464014c1dd0ab2730f64eb6592b0e5
Mxisess <francobernabe0104@gmail.com>
2025-04-17 23:01:09 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Puedes especificar el directorio de salida
-    sourcemap: false, // Si deseas generar los mapas fuente
-    minify: 'terser', // Usar terser para minificar el código
-    target: 'esnext', // Definir el objetivo para la compatibilidad de JS
-  },
 });
