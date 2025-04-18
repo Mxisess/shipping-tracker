e21c579d0603e57bdfe255da7b070f1a3f436338
Mxisess <francobernabe0104@gmail.com>
2025-04-17 20:32:16 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,3 +33,15 @@ function App() {
 }
 
 export default App
+import React from "react";
+import USPSTracker from "./USPSTracker";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <USPSTracker />
+    </div>
+  );
+}
+
+export default App;
